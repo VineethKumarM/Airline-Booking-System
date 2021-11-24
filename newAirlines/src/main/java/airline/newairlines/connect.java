baa1b34fcfd5801bc02caacaf8dff31e53c805e8
VineethKumarM <munigyala@gmail.com>
2021-11-24 21:40:54 +0530

    Server and Code almost done,need UI fixes

---
+++ newAirlines/src/main/java/airline/newairlines/connect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline.newairlines;
+
+/**
+ *
+ * @author Vinee
+ */
+
+import java.sql.*;
+public class connect {
+    Connection c;
+    Statement s;
+    PreparedStatement pst;
+    public connect() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///newairlines","sample","");
+            s =c.createStatement(); 
+         
+        }catch(SQLException e){ 
+            System.out.println(e);
+        }  
+    }
+}
